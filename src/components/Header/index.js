ca83af1a2fe48fe7676331e5d8d20d78892fa21d
Ruzzlan1 <voramzoram1@gmail.com>
2022-03-27 01:54:47 +0400

    Image imported and used in Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import money from "../../assets/images/money.png" 
 export const Header = () => {
   return (
     <header className='header'>
+        <div className="is-flex is-align-items-center">
         <h2 className="is-size-2 has-text-weight-bold">Am I a Millionaire Now?</h2>
+        <img  src={money} className="image is-48x48 ml-4" alt="dollar image" />
+        </div>
+
         <p className='desc-for-app is-size-5'>
             Enter your net worth in USD $ and find out when you'll be a millionaire in currencies all over the world.
         </p>
