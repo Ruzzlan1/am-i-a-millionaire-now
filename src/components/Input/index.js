be9e204b798ab52195a36c7b238e0b522e12601e
Ruzzlan1 <voramzoram1@gmail.com>
2022-03-27 01:51:35 +0400

    Form element added input component
    I did this for submitting changes easily with onSubmit event

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Button } from '../Button'
 export const Input = () => {
   return (
-    <div className='input-container mt-3 is-flex'>
-        <input class="input is-normal" type="number" placeholder="Type your net worth,dont hide it :d"></input>
+    <form className='field form-container mt-3 is-flex'>
+        <input className="input is-normal" type="number" placeholder="Type your net worth,dont hide it :d"></input>
         <Button />
-    </div>
+    </form>
   )
 }
