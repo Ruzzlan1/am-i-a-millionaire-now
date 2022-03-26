c8f4584c8f2aaa98d68e0c49ff928823cb0c5eea
Ruzzlan1 <voramzoram1@gmail.com>
2022-03-27 01:50:57 +0400

    Some typo issue handled

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className='block'>
-        <p className='mt-6'>Made by <a href="https://github.com/Ruzzlan1"><span className='tag'>Ruslan Qurbanov.</span></a>
+        <p className='mt-6'>Made by <a href="https://github.com/Ruzzlan1"><span className='tag'>Ruslan Qurbanov .</span></a>
         Check out the source code on <a href="#">Github!</a>
         </p>
     </footer>
