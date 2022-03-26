9ae60b26c0cb70fd0934d220bd8e620b3cde3b52
Ruzzlan1 <voramzoram1@gmail.com>
2022-03-26 22:54:08 +0400

    Main Page is created
    This is a stateful component for have all the businnes logic and
    context.

---
+++ src/containers/CurrencyTrackPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Footer } from '../../components/Footer'
+import {Header} from "../../components/Header"
+import { Input } from '../../components/Input'
+
+export const CurrencyTrackPage = () => {
+  return (
+    <div className='hero container-my'>
+        <Header />
+        <Input />
+        <Footer />
+    </div>
+  )
+}
