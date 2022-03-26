d66786d80f5080cb3e3264288668c37c5a795526
Ruzzlan1 <voramzoram1@gmail.com>
2022-03-26 22:01:02 +0400

    Header component styled
    I styled header part with sass and bulma classes.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header>Header</header>
+    <header className='header is-flex is-justify-content-center is-align-items-center is-flex-direction-column'>
+        <h2 className="is-size-2">Am I a Millionaire Now?</h2>
+        <p className='desc-for-app'>
+            Enter your net worth in USD $ and find out when you'll be a millionaire in currencies all over the world.
+        </p>
+    </header>
   )
 }
