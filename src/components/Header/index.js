847634d64b493978b4a68bfae9dc42f9e8194f30
Ruzzlan1 <voramzoram1@gmail.com>
2022-03-26 23:06:15 +0400

    Header component styling is done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className='header is-flex is-justify-content-center is-align-items-center is-flex-direction-column'>
-        <h2 className="is-size-2">Am I a Millionaire Now?</h2>
-        <p className='desc-for-app'>
+    <header className='header'>
+        <h2 className="is-size-2 has-text-weight-bold">Am I a Millionaire Now?</h2>
+        <p className='desc-for-app is-size-5'>
             Enter your net worth in USD $ and find out when you'll be a millionaire in currencies all over the world.
         </p>
     </header>
